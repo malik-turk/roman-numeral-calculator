351e8e411d60c257b5dcd9d78b423f5752593c45
ngTurk <malik_alturk@hotmail.com>
2022-11-18 22:18:30 +0200

    implement calculation

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+// Utils
+import { converter } from "utils/converter.utils";
+
 // Styled Components
 const Container = styled.div`
   margin: 0 auto;
@@ -11,6 +14,12 @@ const Container = styled.div`
 const NumericInputLabel = styled.label`
   font-size: 20px;
   color: #161b22;
+`;
+const ErrorLabel = styled.small`
+  font-size: 14px;
+  font-weight: bold;
+  margin-top: 5px;
+  color: #000;
 `;
 const NumericInput = styled.input`
   border: none;
@@ -33,14 +42,17 @@ const RomanNumeralsOutput = styled.div`
 `;
 
 export default function Calculator(): JSX.Element {
-  const [numericValue, setNumericValue] = useState(null);
-  const [romanNumeral, setRomanNumeral] = useState("");
+  const [numericValue, setNumericValue] = useState<number>(0);
+  const [romanNumeral, setRomanNumeral] = useState<string|undefined>("");
 
   /**
    * Handle calculations based on user input
    * @param {ChangeEvent} e type event
    */
-  const handleCalculation = (e: React.ChangeEvent): void => {};
+  const handleCalculation = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setRomanNumeral(converter(parseInt(e.target.value)));
+    setNumericValue(parseInt(e.target.value));
+  };
 
   /**
    * Validate user input
@@ -66,6 +78,9 @@ export default function Calculator(): JSX.Element {
         onKeyDown={validateUserInput}
         placeholder="e.g. 318"
       />
+      {(numericValue < 1 || numericValue > 1000) &&
+        <ErrorLabel>* The number you enter must be between 1 and 1000</ErrorLabel>
+      }
       {romanNumeral && (
         <>
           <hr />
--- utils/converter.utils.ts
+++ utils/converter.utils.ts
@@ -2,8 +2,32 @@
  * Convert integers to roman numerals
  * @param number the number to be converted
  */
-export const converter = (number: number): number => {
-    // code goes here
+export const converter = (number: number): string|undefined => {
+    if (number > 1000 || number < 1) {
+        return;
+    }
 
-    return 2;
+    let result = '';
+    const romanNumeralsMap = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    Object.entries(romanNumeralsMap).forEach(([letter, i]) => {
+        result += letter.repeat(Math.floor(number / i));
+        number %= i;
+    });
+
+    return result;
 };
